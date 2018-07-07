65955db9e3b715e6b4632a557d004784736aab3d
Flavio Maria De Stefano <destefano.flavio@gmail.com>
2018-07-07 14:29:14 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -24,58 +24,57 @@ echo "Install Brew's software base"
 brew install coreutils
 brew install moreutils
 brew install findutils
-brew install curl --force
 brew install gnu-sed --with-default-names
 brew install wget --with-iri
 brew install autoconf
+if [ "$1" == "advanced" ]; then
+brew install curl --force
 brew install imagemagick --with-webp
 brew install graphicsmagick
 brew install gd 
+brew install ffmpeg --with-libvorbis --with-libvpx
+brew install libimobiledevice
 brew install gettext 
 brew install openssl 
 brew install git 
 brew install freetype  
 brew install zlib
-brew install ffmpeg --with-libvorbis --with-libvpx
-brew install libimobiledevice
+fi
 
 echo "Install interpreters"
 brew install node
 brew install nvm
 
-echo "Install Hacking tools"
-brew cask install tunnelblick
-brew cask install impactor
-brew cask install hopper-disassembler
-
 echo "Install Docker"
 brew cask install docker
-brew cask install kitematic
 
 echo "Install Cask base"
+brew cask install tunnelblick
 brew cask install vlc
 brew cask install slack
 brew cask install alfred
 brew cask install telegram
 brew cask install sublime-text
 brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install the-unarchiver
+brew cask install dash
+brew cask install google-chrome
+brew cask install transmit
+brew cask install 1password
+brew cask install sequel-pro
+brew cask install google-drive
+brew cask install spotify
+brew cask install cloudapp
+if [ "$1" == "advanced" ]; then
 brew cask install torbrowser
 brew cask install virtualbox
 brew cask install virtualbox-extension-pack
 brew cask install charles
 brew cask install evernote
 brew cask install gpgtools
-brew cask install iterm2
-brew cask install dash
 brew cask install sqlpro-for-sqlite
-brew cask install the-unarchiver
-brew cask install transmit
-brew cask install google-chrome
 brew cask install firefox
-brew cask install 1password
-brew cask install sequel-pro
-brew cask install google-drive
-brew cask install spotify
 brew cask install transmission
 brew cask install sketch
 brew cask install paw
@@ -83,8 +82,8 @@ brew cask install poedit
 brew cask install sip
 brew cask install hex-fiend
 brew cask install studio-3t
-brew cask install cloudapp
 brew cask install sourcetree
+fi
 
 echo "Install Brew caskromm fonts"
 brew tap caskroom/fonts
