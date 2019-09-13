8d090e9b50f25f326c6f70aca64522e96f53ba75
Flavio De Stefano <destefano.flavio@gmail.com>
2019-09-13 10:00:54 +0200

    Adding submodules

--- conf-linker.sh
+++ conf-linker.sh
@@ -3,4 +3,6 @@ set -ex
 ln -sf /opt/dotfiles/conf/bash_profile.sh ~/.bash_profile
 ln -sf /opt/dotfiles/conf/eslintrc.json ~/.eslintrc.json
 ln -sf /opt/dotfiles/conf/editorconfig.txt ~/.editorconfig
+
+rm -rf ~/.oh-my-zsh/custom/plugins ~/.oh-my-zsh/custom/themes
 ln -sf /opt/dotfiles/oh-my-zsh/* ~/.oh-my-zsh/custom/
