4f8bfcdf833ad00196d3a03613ee059222362b88
Flavio De Stefano <destefano.flavio@gmail.com>
2020-09-19 16:27:57 +0200

    Renaming files

--- extra.sh
+++ extra.sh
@@ -9,11 +9,6 @@ ln -sf ~/.dotfiles/conf/zshrc.sh ~/.zshrc
 ln -sf ~/.dotfiles/conf/bash_profile.sh ~/.bash_profile
 ln -sf ~/.dotfiles/conf/gitignore_global.txt ~/.gitignore_global
 
-echo "Configuring GPG"
-mkdir -p ~/.gnupg
-grep "pinentry-program" ~/.gnupg/gpg-agent.conf || echo "pinentry-program /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
-grep "no-tty"  ~/.gnupg/gpg.conf || echo "no-tty" >> ~/.gnupg/gpg.conf
-
 echo "Installing iTerm shell integration"
 curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
 
