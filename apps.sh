b97cf8400f8b243eb08b87eb54abd8555497f02a
Flavio De Stefano <destefano.flavio@gmail.com>
2020-11-25 16:03:04 +0100

    Adding cleaning

--- apps.sh
+++ apps.sh
@@ -3,6 +3,8 @@
 echo "Installing and upgrading Brew"
 [ -s "/usr/local/bin/brew" ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew analytics off
+
 echo "Tapping Brew's"
 for e in $(cat brewtap.txt); do brew tap $e; done
 brew update
@@ -17,5 +19,6 @@ echo "Install MAS's apps"
 brew install mas
 for e in $(cat mas.txt); do mas install $e; done
 
-echo "Cleaning Brew"
+echo "Cleaning"
+qlmanage -r
 brew cleanup
