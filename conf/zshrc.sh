17e2bfe5eb9ab64b96e942db08ad8a72cbcbe4e9
Flavio De Stefano <destefano.flavio@gmail.com>
2020-11-25 17:09:26 +0100

    Fixing -f

--- conf/zshrc.sh
+++ conf/zshrc.sh
@@ -1,5 +1,5 @@
-source ~/.bash_profile
-source ~/.bash_profile_custom
+[ -f ~/.bash_profile ] && source ~/.bash_profile
+[ -f ~/.bash_profile_custom ] && source ~/.bash_profile_custom
 
 export ZSH=~/.oh-my-zsh
-source $ZSH/oh-my-zsh.sh
+[ -f $ZSH/oh-my-zsh.sh ] && source $ZSH/oh-my-zsh.sh
