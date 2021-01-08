102129c601d6c7ea2aa04f6a6fee98b971ff3ecf
Flavio De Stefano <destefano.flavio@gmail.com>
2021-01-08 14:16:11 +0100

    dir str

--- apps.sh
+++ apps.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "Installing and upgrading Brew"
-[ -s "/usr/local/bin/brew" ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+[ -s "/usr/local/bin/brew" ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homeapps/install/master/install)"
 
 brew analytics off
 
 echo "Tapping Brew's"
-for e in $(cat brewtap.txt); do brew tap $e; done
+for e in $(cat ./_apps/brewtap.txt); do brew tap $e; done
 brew update
 
 echo "Install Brew's software base"
-for e in $(cat brewlist.txt); do brew install $e; done
+for e in $(cat ./_apps/brewlist.txt); do brew install $e; done
 
 echo "Install Cask's base"
-for e in $(cat brewcasklist.txt); do brew cask install $e; done
+for e in $(cat ./_apps/brewcasklist.txt); do brew cask install $e; done
 
 echo "Install MAS's apps"
 brew install mas
-for e in $(cat mas.txt); do mas install $e; done
+for e in $(cat ./_apps/mas.txt); do mas install $e; done
 
 echo "Cleaning"
 qlmanage -r
